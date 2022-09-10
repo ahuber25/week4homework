2c3cd845c4db733045d082ea03a939bfe158e9fe
Scotch <horsinaround235@gmail.com>
2022-09-10 09:24:15 -0400

    added questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Where can you put the external script call in your HTML?",
+        choices: ["In the head of the page", "At the bottom of the body", "Either the head or the body", "None of the above"],
+        answer: "Either the head or the body"
+    },
+    {
+        title: "How do you write a comment in a script file?",
+        choices: ["//This is a comment", "/*This is a comment*/", "<!--This is a comment-->", "The first and second are correct"],
+        answer: "The first and second are correct"
+    },
+    {
+        title: "How do you intentionally assign no value to a variable?",
+        choices: ["Undefined", "NaN", "Null", "None of the above"],
+        answer: "Null"
+    },
+    {
+        title: "What is the correct way to write an array?",
+        choices: ["var num=(1,2,3)", "var num=[1,2,3]"],
+        answer: "var num=[1,2,3]"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+]
